a0656ca7206ea77541de6fc252862346b7cec470
Christopher O'Brien <chris@theobriens.co.nz>
2020-12-04 10:35:03 +1300

    get file reading added

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var fileTextLines []string
+
+	for fileScanner.Scan() {
+		fileTextLines = append(fileTextLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+
+	for _, line := range fileTextLines {
+		fmt.Println(line)
+	}
+
+}
