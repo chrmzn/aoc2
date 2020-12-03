e04f68a22fd8a7f2dd6e3661ab4d9c9734bc170d
Christopher O'Brien <chris@theobriens.co.nz>
2020-12-04 10:54:48 +1300

    add a splitter and get mix and max counts

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
+// It's listed as a left/right splitter but it's really returning first and second...
 func splitter(str string, sep string) (left, right string) {
-
+	stringSplit := strings.Split(str, sep)
+	left, right = stringSplit[0], stringSplit[1]
+	return left, right
 }
 
 func main() {
@@ -30,13 +34,24 @@ func main() {
 
 	for _, line := range fileTextLines {
 		// fmt.Println(line)
-		stringSplit := strings.Split(line, ": ")
-		match, password := stringSplit[0], stringSplit[1]
-		fmt.Printf("Match: '%s'; Password: '%s'\n", match, password)
+		matcher, password := splitter(line, ": ")
+		fmt.Printf("Match: '%s'; Password: '%s'\n", matcher, password)
 
-		stringSplit = strings.Split(match, " ")
-		valueCount, valueMatch := stringSplit[0], stringSplit[1]
-		fmt.Printf("Counts: '%s'; Matcher: '%s'\n", valueCount, valueMatch)
+		valueCounts, valueMatch := splitter(matcher, " ")
+		fmt.Printf("Counts: '%s'; Matcher: '%s'\n", valueCounts, valueMatch)
+
+		minCountStr, maxCountStr := splitter(valueCounts, "-")
+		fmt.Printf("MinCount: '%s'; MaxCount: '%s'\n", minCountStr, maxCountStr)
+
+		minCount, err := strconv.Atoi(minCountStr)
+		if err != nil {
+			panic(err)
+		}
+		maxCount, err := strconv.Atoi(maxCountStr)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("MinCount: %d; MaxCount: %d\n", minCount, maxCount)
 
 		fmt.Println("")
 	}
